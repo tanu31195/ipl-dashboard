6a609fd099c0b4fb885aa32af6b1ba39b5c61795
tanu31195 <11332345+tanu31195@users.noreply.github.com>
2021-05-24 02:20:14 +0530

    Initial commit

---
+++ src/test/java/io/github/tanu31195/ipldashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tanu31195.ipldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
