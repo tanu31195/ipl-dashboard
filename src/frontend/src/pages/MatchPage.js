7fffe3364da57f19661fb4819fc90a0080dd1fc4
tanu31195 <11332345+tanu31195@users.noreply.github.com>
2021-05-31 23:46:09 +0530

    React: Added YearSelector

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,12 +1,15 @@
 /*
  * Created by Tanushka Bandara (https://tanu31195.github.io)
- * Last Modified on 5/30/21, 12:00 AM
+ * Last Modified on 5/31/21, 10:39 PM
  * Copyright (c) 2021. All rights reserved.
  */
 
 import {React, useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
 import {MatchDetailCard} from "../components/MatchDetailCard";
+import {YearSelector} from "../components/YearSelector";
+
+import "./MatchPage.scss"
 
 export const MatchPage = () => {
     const [matches, setMatches] = useState([]);
@@ -20,15 +23,21 @@ export const MatchPage = () => {
                 setMatches(data);
             };
             fetchMatches();
-        }, [teamName,year]
+        }, [teamName, year]
     );
     if (matches.length === 0) {
         return <h1>No matches found for {teamName}</h1>
     }
     return (
         <div className="MatchPage">
-            <h1>Match Page</h1>
-            {matches.map(match => <MatchDetailCard key={match.id} match={match} teamName={teamName}/>)}
+            <div className="year-selector">
+                <h3>Select year</h3>
+                <YearSelector teamName={teamName}/>
+            </div>
+            <div>
+                <h1 className="page-heading">Matches of {teamName} for the year of {year}</h1>
+                {matches.map(match => <MatchDetailCard key={match.id} match={match} teamName={teamName}/>)}
+            </div>
         </div>
     );
 }
